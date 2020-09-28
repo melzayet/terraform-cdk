ac8dfff3f1e6687344e9e78684e296d0c78d9f00
Mahmoud El Zayet <Mahmoud.Elzayet@microsoft.com>
2020-09-28 18:08:15 +0000

    re

--- examples/typescript/azure/main.ts
+++ examples/typescript/azure/main.ts
@@ -1,13 +1,9 @@
 import { App } from 'cdktf';
 import { ContosoContainerStack } from "./ContosoContainerStack"
-//import { ContosoCosmosSqlFreeTierStack } from "./ContosoCosmosSqlFreeTierStack"
 
 const app = new App();
 
 //Create container stack from Contoso wide shared library
 new ContosoContainerStack(app, 'my-contoso-container-stack');
-
-//Create free tier cosmos db stack from Contoso wide shared library
-//new ContosoCosmosSqlFreeTierStack(app, 'my-cosmos-sql-free-tier-stack');
 
 app.synth();
